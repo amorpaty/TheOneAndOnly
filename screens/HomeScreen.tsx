42b654f8375e8c298552a8ce2727d878e81974db
ehdrmfkal0624@naver.com <ehdrmfkal0624@naver.com>
2024-09-25 12:14:58 +0900

    android 구글 지도 연동

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+/**
+ * 24.09.24 최초생성
+ * Home 화면 
+ * 구글 지도 연동
+ * @returns 
+ */
 
 function HomeScreen() {
-    return (<></>)
+    return (
+        <View style={{ flex: 1 }}>
+            <MapView    
+            style={{ flex: 1 }}
+            provider={PROVIDER_GOOGLE}
+            initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }}
+            />
+        </View>
+    )
     
 }
 
