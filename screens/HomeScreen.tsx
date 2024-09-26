d5bbf8af3b4dcb3ae0e0b0b933b8322b723c10d2
ehdrmfkal0624@naver.com <ehdrmfkal0624@naver.com>
2024-09-26 19:44:49 +0900

    Home 화면 메뉴, 위치 버튼 생성

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { View } from "react-native";
+import { Alert, View } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import FloatingLocationButton from "../components/FloatingLocationButton";
+import Geolocation from 'react-native-geolocation-service'; // 현재 위치 추적
+import FloatCafeListButton from "../components/FloatingCafeListButton";
+
 
 /**
  * 24.09.24 최초생성
@@ -9,22 +13,33 @@ import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
  * @returns 
  */
 
+const defaultPosition = {
+    latitude: 35.91395373474155,
+    longitude: 127.73829440215488,
+    latitudeDelta: 3,
+    longitudeDelta: 3,
+}
+
+//TODO
+//지도 현재 위치 조회 컴포넌트 구현
+function handleOnClick(){
+
+}
+
 function HomeScreen() {
     return (
         <View style={{ flex: 1 }}>
             <MapView    
-            style={{ flex: 1 }}
-            provider={PROVIDER_GOOGLE}
-            initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-            }}
+                style={{ flex: 1 }}
+                provider={PROVIDER_GOOGLE}
+                initialRegion={defaultPosition}
+                showsUserLocation={true}
+                showsMyLocationButton={true}
             />
+            <FloatCafeListButton />
+            <FloatingLocationButton/>
         </View>
     )
-    
 }
 
 export default HomeScreen;
